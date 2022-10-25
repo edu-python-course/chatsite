761b6b24b72a84f83a58cd2fdc821eff642d5619
Serhii Horodilov <sgorodil@gmail.com>
2022-10-25 18:56:54 +0300

    Added channels support
    Added channels_redis package to project's deps.
    Applied channels layer configuration.
    Updated chat room consumer.

--- chatroom/consumers.py
+++ chatroom/consumers.py
@@ -2,8 +2,10 @@
 Chatroom application consumer
 
 """
+
 import json
 
+from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
 
 
@@ -11,13 +13,28 @@ class ChatRoomConsumer(WebsocketConsumer):
     """Chat room websocket consumer implementation"""
 
     def connect(self):
+        self.room_id = self.scope["url_route"]["kwargs"]["room_id"]
+        self.room_group_name = "chat_%s" % self.room_id
+
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_group_name, self.channel_name
+        )
+
         self.accept()
 
     def disconnect(self, code):
-        pass
+        async_to_sync(self.channel_layer.group_discard)(
+            self.room_group_name, self.channel_name
+        )
 
     def receive(self, text_data=None, bytes_data=None):
         payload = json.loads(text_data)
         message = payload["message"]
 
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name, {"type": "chat_message", "message": message}
+        )
+
+    def chat_message(self, event):
+        message = event["message"]
         self.send(text_data=json.dumps({"message": message}))
